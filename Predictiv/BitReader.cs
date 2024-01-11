86beff50483f2ccaf3d2599cf028e20e0da4ca61
andumihailescu <contact.andumihailescu@gmail.com>
2024-01-11 20:48:01 +0200

    Project initialization

---
+++ Predictiv/BitReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Metadata;
+
+namespace Bit_Reader_Writer
+{
+    internal class BitReader
+    {
+        private byte BufferReader;
+        private int NumberOfBitsToRead;
+        private FileStream inputFileStream;
+
+        public BitReader(string inputFilePath)
+        {
+            inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+            NumberOfBitsToRead = 0;
+        }
+
+        public long GetFileLengthInBytes()
+        {
+            return inputFileStream.Length;
+        }
+
+        private bool IsBufferEmpty()
+        {
+            if (NumberOfBitsToRead == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CloseFile()
+        {
+            inputFileStream.Flush();
+            inputFileStream.Close();
+            inputFileStream.Dispose();
+        }
+
+        private byte ReadBit()
+        {
+            if (IsBufferEmpty())
+            {
+                BufferReader = (byte)inputFileStream.ReadByte();
+                NumberOfBitsToRead = 8;
+            }
+            byte result = (byte)((BufferReader >> (NumberOfBitsToRead - 1)) & 1);
+            NumberOfBitsToRead--;
+            return result;
+        }
+        public uint ReadNBits(uint nr)
+        {
+            uint result = 0;
+            for (int i = 0; i < nr; i++)
+            {
+                byte bit = ReadBit();
+                if (i != 0)
+                {
+                    result = result << 1;
+                }
+                result += bit;
+            }
+            return result;
+        }
+    }
+}
+
---
+++ Predictiv/BitWriter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Reader_Writer
+{
+    internal class BitWriter
+    {
+        private byte BufferWriter;
+        private int NumberOfBitsToWrite;
+        private FileStream outputFileStream;
+        public BitWriter(string outputFilePath)
+        {
+            outputFileStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+            BufferWriter = 0;
+            NumberOfBitsToWrite = 8;
+        }
+
+
+        public void CloseFile()
+        { 
+            outputFileStream.Flush();
+            outputFileStream.Close();
+            outputFileStream.Dispose();
+        }
+        private bool IsBufferFull()
+        {
+            return NumberOfBitsToWrite == 0;
+        }
+
+        private void WriteBit(byte value)
+        {
+            if (NumberOfBitsToWrite != 8)
+            {
+                BufferWriter = (byte)(BufferWriter << 1);
+            }
+            BufferWriter += value;
+            NumberOfBitsToWrite--;
+
+            if (IsBufferFull())
+            {
+                NumberOfBitsToWrite = 8;
+                outputFileStream.WriteByte(BufferWriter);
+                Console.WriteLine(BufferWriter);
+                BufferWriter = 0;
+            }
+        }
+        public void WriteNBits(uint nr, uint value)
+        {
+            for(int i = 0; i < nr; i++)
+            {
+                byte bit = (byte)((value >> (int)(nr - i - 1)) & 1);
+                WriteBit(bit);
+            }
+        }
+    }
+}
